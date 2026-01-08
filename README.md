0bf360ed91da2020d1cacc14bdfd2275f436b3dc
chLi <565353780@qq.com>
2026-01-08 10:37:01 +0800

    create setup bash files

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
-# cd-gs
+# CDGS
+
+## Setup
+
+```bash
+conda create -n cdgs python=3.10
+conda activate cdgs
+./setup.sh
+```
+
+## Run
+
+```bash
+./train.sh
+```
+
+## Enjoy it~
---
+++ setup.sh
@@ -0,0 +1,18 @@
+cd ..
+git clone --depth 1 https://github.com/facebookresearch/pytorch3d.git
+
+pip install ninja
+
+pip3 install torch torchvision torchaudio \
+  --index-url https://download.pytorch.org/whl/cu128
+
+pip install plyfile tqdm matplotlib opencv-python joblib timm
+
+cd pytorch3d
+python setup.py install
+
+cd ../cd-gs/submodules/diff-gaussian-rasterization-depth-acc
+python setup.py install
+
+cd ../simple-knn
+python setup.py install
